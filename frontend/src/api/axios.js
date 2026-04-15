dbe605ef02feb2cb345752458db36ccdbe79b9e7
Kart8931 <ashukla1171@gmail.com>
2026-04-15 20:00:41 +0530

    changes and deploy

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://imagestore-2.onrender.com',
 });
 
 // Har request mein token apne aap chala jaye, uske liye interceptor
